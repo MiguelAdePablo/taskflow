444452908c37ca932c41fd938ecaafb6d2b8ec7b
MA de Pablo <madepablo@outlook.es>
2026-08-30 17:46:07 +0200

    Update

--- backend/run.py
+++ backend/run.py
@@ -1,8 +1,15 @@
 from app import create_app, socketio
 
-# Crear la aplicación Flask
-app = create_app()
+# # Crear la aplicación Flask
+# app = create_app()
 
+# if __name__ == '__main__':
+#     # Ejecutar el servidor con SocketIO para soportar WebSockets
+#     socketio.run(app, debug=True, host='0.0.0.0', port=5000)
+
+from gevent import monkey
+monkey.patch_all()
+from app import create_app, socketio
+app = create_app()
 if __name__ == '__main__':
-    # Ejecutar el servidor con SocketIO para soportar WebSockets
     socketio.run(app, debug=True, host='0.0.0.0', port=5000)
