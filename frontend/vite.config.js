affa8c7bd407669744dfb7f2895b4928106e5248
MA de Pablo <madepablo@outlook.es>
2026-08-30 16:57:09 +0200

    Update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,40 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://taskflow-api-852f.onrender.com',
+        changeOrigin: true,
+        secure: false,
+      },
+      '/socket.io': {
+        target: 'https://taskflow-api-852f.onrender.com',
+        changeOrigin: true,
+        secure: false,
+        ws: true,
+      }
+    }
+  },
+  preview: {
+    proxy: {
+      '/api': {
+        target: 'https://taskflow-api-852f.onrender.com',
+        changeOrigin: true,
+        secure: false,
+      },
+      '/socket.io': {
+        target: 'https://taskflow-api-852f.onrender.com',
+        changeOrigin: true,
+        secure: false,
+        ws: true,
+      }
+    }
+  }
 })
